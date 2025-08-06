76181a5a95fa23f6330d65c69add6027adfcfdd5
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2025-08-06 11:18:32 +0200

    adding helper functions to construct indicator AD-compatible

--- R/aaa_utils.R
+++ R/aaa_utils.R
@@ -20,15 +20,35 @@ NULL
 #' @export
 #' @importFrom RTMB pnorm
 erf <- function(x) {
-  2 * RTMB::pnorm(x * sqrt(2)) - 1
+  2 * RTMB::pnorm(x * sqrt(2)) - 1 # + eps
 }
 #' @rdname erf
 #' @export
 erfc <- function(x) {
-  1 - erf(x)
+  1 - erf(x) # + eps
 }
 
 ## AD pmin/pmax helpers that work for both ad and numeric:
 pmin.ad <- function(x, y) apply(cbind(x,y), 1, min)
 pmax.ad <- function(x, y) apply(cbind(x,y), 1, max)
+
+## AD-indicator constructors
+# 1 if x == 0, 0 otherwise
+iszero <- function(x) {
+  1 - sign(x)^2
+}
+# 1 if x > 0, 0 otherwise
+ispos <- function(x) {
+  s <- sign(x)
+  0.5 * (s + abs(s))
+}
+# 1 if x < val, 0 otherwise
+smaller <- function(x, val) {
+  s <- sign(x - val)
+  0.5 * (abs(s) - s)
+}
+# 1 if x > val, 0 otherwise
+greater <- function(x, val) {
+  1 - smaller(x, val)
+}
 
